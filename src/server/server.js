2368d3bf8515fd8271d4a8ff4ea39d58816191a8
RajChoudhary007007 <raaz02256@gmail.com>
2025-06-28 19:48:36 +0530

    server url

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 // server.js
 // const SERVER_API_URL = 'https://dc.dceyewr.com'; 
-const SERVER_API_URL = 'http://localhost:8000'; 
+// const SERVER_API_URL = 'http://localhost:8000'; 
+const SERVER_API_URL = 'http://backend.bizzcountsolutions.com'; 
 
 export { SERVER_API_URL };
